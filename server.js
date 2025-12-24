797b652f51b8f670bae52625d71f80c978df4ea7
Jabaraj raju <jabarajraju1590@gmail.com>
2025-12-24 13:09:34 +0530

    backend

--- server.js
+++ server.js
@@ -6,16 +6,30 @@ const atsRoutes = require('./routes/atsRoutes');
 const authRoutes = require('./routes/authRoutes');
 const resumeRoutes = require('./routes/resumeRoutes');
 
+const fs = require('fs');
+const path = require('path');
+
 dotenv.config();
 connectDB();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+// Middleware
 app.use(cors());
 app.use(express.json());
 
+// Ensure uploads directory exists
+const uploadDir = path.join(__dirname, 'uploads');
+if (!fs.existsSync(uploadDir)) {
+    fs.mkdirSync(uploadDir);
+}
+
 // Routes
+app.get('/', (req, res) => {
+    res.send("Server is Running");
+});
+
 app.use('/api/ats', atsRoutes);
 app.use('/api/auth', authRoutes);
 app.use('/api/resume', resumeRoutes);
